f0b72d60b69329f7126d8eeadf683b2a705f1d50
Napp <83326679+AetCloud@users.noreply.github.com>
2025-03-07 21:37:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const {
   getLastPostedImage,
   saveLastPostedImage,
   getWalltakerSettings,
-  fetchWalltakerSettings,
 } = require("./commands/setwalltaker.js");
 
 const TOKEN = process.env.TOKEN;
